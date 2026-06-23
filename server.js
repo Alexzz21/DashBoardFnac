78eac8b94c2022544e1696f58f91d42375a2b249
Alexzz21 <arodri461@gmail.com>
2026-06-23 16:44:29 -0500

    Fix ES module syntax

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Proxy API calls to Odoo
 app.use('/odoo-api', createProxyMiddleware({
   target: 'https://javier-vela.odoo.com',
   changeOrigin: true,
   pathRewrite: { '^/odoo-api': '' },
 }));
 
-// Proxy API calls to Fnac
 app.use('/fnac-api', createProxyMiddleware({
   target: 'https://vendeur.fnac.com',
   changeOrigin: true,
   pathRewrite: { '^/fnac-api': '' },
 }));
 
-// Proxy API calls to Casa del Libro
 app.use('/cdl-api', createProxyMiddleware({
   target: 'https://casadellibro-prod.mirakl.net',
   changeOrigin: true,
   pathRewrite: { '^/cdl-api': '' },
 }));
 
-// Serve React build
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// All other routes serve React app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
